0af58fccd5974886a0adb24205968d06f3736af3
David Leyva <dleyvaabrahantes@gmail.com>
2023-06-02 15:41:42 -0400

    Initial Commit

---
+++ CustomSheet/CustomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSheetApp.swift
+//  CustomSheet
+//
+//  Created by David on 6/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
